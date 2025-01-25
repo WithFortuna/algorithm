7d366564e554eb3b8206690f00a596cb7d86617d
GNO <rmsghchl0@naver.com>
2025-01-25 13:57:58 +0900

    20250125_1일_1알골

---
+++ studio/oxo2_3/oxo2_3/17478.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int N;
+void displayDash(int n)	//n개만큼 dash출력
+{
+	while (n--)
+	{
+		cout << '_';
+	}
+}
+void recFunc(int k, int dash)
+{
+	//탈출조건
+	if (k == 0)
+	{
+		//질
+		displayDash(dash);
+		cout << "\"재귀함수가 뭔가요?\""<<'\n';
+		//답2
+		displayDash(dash);
+		cout << "\"재귀함수는 자기 자신을 호출하는 함수라네\"" << '\n';
+		//끝
+		displayDash(dash);
+		cout << "라고 답변하였지." << '\n';
+
+	}
+	else
+	{
+		//질
+		displayDash(dash);
+		cout << "\"재귀함수가 뭔가요?\"" << '\n';
+		//답
+		displayDash(dash);
+		cout << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어." << '\n';
+		displayDash(dash);
+		cout << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지." << '\n';
+		displayDash(dash);
+		cout << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\""<<'\n';
+
+		recFunc(k-1, dash + 4);
+		//끝
+		displayDash(dash);
+		cout << "라고 답변하였지." << '\n';
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> N;
+	cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다."<<'\n';
+	recFunc(N, 0);
+
+	return 0;
+}
